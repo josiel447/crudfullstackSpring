e19a03bfe01e2ed234f7321fa0f99d57a678eaba
João Pedro de Sousa Pacheco <joaopach22@gmail.com>
2025-11-02 18:46:41 -0300

    Add files via upload

---
+++ Aplica-o_erro-main/aula20_10-main/crudfullstackSpring-alteracaoBancoDados/backend/src/main/java/com/exemplo/crudmongo/controller/CursoController.java
@@ -0,0 +1,50 @@
+package com.exemplo.crudmongo.controller;
+
+import com.exemplo.crudmongo.Model.Curso;
+import com.exemplo.crudmongo.service.CursoService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController 
+@RequestMapping("/api/curso") 
+@CrossOrigin(origins = "*") 
+
+public class CursoController {
+     private final CursoService service; 
+
+   
+    public CursoController(CursoService service) {
+        this.service = service;
+    }
+
+  
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ALUNO', 'COORDENADOR')")
+    public List<Curso> listar() {
+        return service.listarTodas();
+    }
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('COORDENADOR')")
+    public Curso criar(@RequestBody Curso curso) {
+        return service.salvar(curso);
+    }
+
+ 
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('COORDENADOR')")
+    public Curso atualizar(@PathVariable Long id, 
+    @RequestBody Curso curso) {
+        return service.atualizar(id, curso);
+    }
+
+   
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('COORDENADOR')")
+    public void excluir(@PathVariable Long id) {
+        service.excluir(id);
+    }
+}
